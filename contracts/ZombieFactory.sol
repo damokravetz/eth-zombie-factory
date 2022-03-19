5bb9daa00bd21b2ad5663e329749064c817856dd
damokravetz <kravetzdamian@gmail.com>
2022-03-19 00:59:58 -0300

    changes

---
+++ contracts/ZombieFactory.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.7.3;
+
+contract ZombieFactory {
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+
+    
+}
